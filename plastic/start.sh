04982af2e7459df86fbb644b64dad9a9ecb6758c
Mikael Kalms <mikael@kalms.org>
2017-03-07 23:27:59 +0100

    Ensure id_rsa file has appropriate permissions

--- plastic/start.sh
+++ plastic/start.sh
@@ -2,5 +2,10 @@
 # Ensure .ssh folder has appropriate permissions, even if the container is built on Windows
 chmod 700 .ssh
 
+# Ensure id_rsa file has appropriate permissions, even if the container is built on Windows
+if [ "$(ls -A /conf/id_rsa)" ]; then
+  chmod 700 /conf/id_rsa
+fi
+
 # Launch Plastic server in daemon mode
 /opt/plasticscm5/server/plasticd --daemon
