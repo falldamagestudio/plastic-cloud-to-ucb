c28bb868d61b622142645ec9351c1518e0e09f21
Mikael Kalms <mikael@kalms.org>
2017-03-10 07:43:06 +0100

    Use profiles.conf instead of client.conf for Plastic Cloud authentication details

--- commands/configure.sh
+++ commands/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if test ! -e "client.conf"; then
-  echo "You need to manually fetch client.conf from the Plastic user-local files folder (typically C:\Users\<username>\AppData\Local\plastic4) and place it in your current dir, before running this script"
+if test ! -e "profiles.conf"; then
+  echo "You need to manually fetch profiles.conf from the Plastic user-local files folder (typically C:\Users\<username>\AppData\Local\plastic4) and place it in your current dir, before running this script"
   exit 1
 fi
 
@@ -14,7 +14,7 @@ fi
 
 # Create authentication file for when the local Plastic server is going to connect to Plastic Cloud
 echo "LDAPWorkingMode" > authentication.conf
-grep -E -m 1 -o "<SecurityConfig>(.*)</SecurityConfig>" client.conf | sed -e 's,.*<SecurityConfig>\([^<]*\)</SecurityConfig>.*,\1,g' >> authentication.conf
+grep -E -m 1 -o "<SecurityConfig>(.*)</SecurityConfig>" profiles.conf | sed -e 's,.*<SecurityConfig>\([^<]*\)</SecurityConfig>.*,\1,g' >> authentication.conf
 docker cp authentication.conf plastic:/conf
 
 # Set up "git" user SSH account
